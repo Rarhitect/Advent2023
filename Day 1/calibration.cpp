0d22ec7abc48fb7cee2dfc5b4c7afd9a2a4a57c6
Rarhitect <74362664+Rarhitect@users.noreply.github.com>
2023-12-01 20:54:04 +0100

    Puzzle 2 solved

--- Day 1/calibration.cpp
+++ Day 1/calibration.cpp
@@ -4,29 +4,83 @@
 #include <fstream>
 
 int line_value(const std::string& line){
-    std::vector<int> digits;
+
+    std::vector<std::string> substring_digits = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    std::vector<std::string> subchar_digits = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+
+    std::vector<std::vector<int> > digits; // containing a pair: position in line and digit itself
     int value = 0;
-    for (auto element: line){
-        if((int)element >= 48 and (int)element <= 57){
-            int int_element = element - '0';
-            digits.push_back(int_element);
+
+    //Finding digits that are written as words
+    for (auto i = 0; i < substring_digits.size(); i++){
+
+        std::string line_copy = line;
+        int found_pos_original = line.find(substring_digits[i]);
+        int found_pos_in_copy = found_pos_original;
+        int counter_of_words = 0;
+
+        while (found_pos_in_copy != std::string::npos){
+            counter_of_words++;
+            std::vector<int> digit{found_pos_original, i+1};
+            digits.push_back(digit);
+            line_copy.erase(found_pos_in_copy, substring_digits[i].length());
+            if (line_copy.find(substring_digits[i]) != std::string::npos){
+                found_pos_original = line_copy.find(substring_digits[i]) + substring_digits[i].length() * counter_of_words;
+                found_pos_in_copy = line_copy.find(substring_digits[i]);
+            }
+            else{
+                break;
+            }
         }
     }
+    // Finding digits that are written as numbers
+    for (auto i = 0; i < subchar_digits.size(); i++){
+
+        std::string line_copy = line;
+        int found_pos_original = line.find(subchar_digits[i]);
+        int found_pos_in_copy = found_pos_original;
+        int counter_of_digits = 0;
+
+        while (found_pos_in_copy != std::string::npos){
+            counter_of_digits++;
+            std::vector<int> digit{found_pos_original, i+1};
+            digits.push_back(digit);
+            line_copy.erase(found_pos_in_copy, subchar_digits[i].length());
+            if (line_copy.find(subchar_digits[i]) != std::string::npos){
+                found_pos_original = line_copy.find(subchar_digits[i]) + subchar_digits[i].length() * counter_of_digits;
+                found_pos_in_copy = line_copy.find(subchar_digits[i]);
+            }
+            else{
+                break;
+            }
+        }
+    }
+
+    // OLD VERSION FROM PUZZLE №1
+    // for (auto element: line){
+    //     if((int)element >= 48 and (int)element <= 57){
+    //         int int_element = element - '0';
+    //         int position = line.find(element);
+    //         std::vector<int> digit = {position, int_element};
+    //         digits.push_back(digit);
+    //     }
+    // }
+
+
+    //Calculating the first and last digit and constructing a calibration value
+    std::sort(digits.begin(), digits.end());
+
     if (sizeof(digits) > 0){
-        /*for (auto & digit: digits){
-            std::cout << digit << " ";
-        }*/
-        //std::cout << std::endl;
-        //std::cout << "First and last digits are:" << digits[0] << " " << digits[digits.size()-1] << std::endl;
-        value = value + digits[0] * 10 + digits[digits.size()-1];
+        value = value + digits[0][1] * 10 + digits[digits.size()-1][1];
     }
     return value;
 }
 
 int main(){
-    std::fstream input_file;
-    input_file.open("input.txt", std::ios::in);
     int calibration_value = 0;
+
+    std::fstream input_file;
+    input_file.open("input.txt", std::ios::in); //input2.txt was used to look through some tests
     if (input_file.is_open()){ //checking whether the file is open
         std::string line;
         while(getline(input_file, line)){ //read data from file object and put it into string.
@@ -39,6 +93,7 @@ int main(){
       }
       input_file.close(); //close the file object.
    }
-   std::cout << "Finally CL = " << calibration_value << std::endl;
 
+    std::cout << "Finally CL = " << calibration_value << std::endl;
+    return 0;
 }
